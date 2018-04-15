2347fa1b78afcb30a244fa095bb8abc43b58fcfd
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-15 13:29:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,15 @@ if((message.author.id === "290111320857116672")  && (message.attachments.first()
  await message.author.send("https://discord.gg/3mYxFKC")
  await message.member.kick()
 }
-  if((message.author.id === "290111320857116672")  && (message.content.includes("http"))) {
+  if((message.author.id === "290111320857116672")  && (message.content.includes("http://"))) {
+ await message.author.send("https://discord.gg/3mYxFKC")
+ await message.member.kick()
+}
+   if((message.author.id === "290111320857116672")  && (message.content.includes("https://"))) {
+ await message.author.send("https://discord.gg/3mYxFKC")
+ await message.member.kick()
+}
+if((message.author.id === "290111320857116672")  && (message.content.includes(".png"))) {
  await message.author.send("https://discord.gg/3mYxFKC")
  await message.member.kick()
 }
